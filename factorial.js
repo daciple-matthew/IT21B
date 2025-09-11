ead8aef6ead6096c66450bb96e9c5fdc72eec49a
daciple-matthew <daciplerosemateo@gmail.com>
2025-09-11 20:08:54 +0800

    Added conditions

--- factorial.js
+++ factorial.js
@@ -17,3 +17,14 @@ console.log("---------------------------");
 rl.question("Enter your choice (1-3): ", choice => {
     console.log(choice);
 })
+
+if (choice === "1"){
+        console.log("Hello");
+    } else if (choice === "2"){
+        console.log("Factorial");
+    } else if (choice === "3"){
+        console.log("Exiting Application");
+    } else {
+        console.log("Invalid Choice. Please Try Again");
+    }
+    
