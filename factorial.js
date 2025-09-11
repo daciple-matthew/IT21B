c93f639a6fbfa51d8fd0a70fbc4d5e2939c8f8a9
daciple-matthew <daciplerosemateo@gmail.com>
2025-09-11 20:41:51 +0800

    Added computeFactorial() function and initial instance

--- factorial.js
+++ factorial.js
@@ -21,29 +21,7 @@ function mainMenu() {
         if (choice === "1") {
             sayHello();
         } else if (choice === "2") {
-            console.log("Factorial");
-
-            rl.question("Please enter a number for factorial: ", numStr => {
-                let num = parseInt(numStr);
-
-                if (isNaN(num) || num < 0) {
-                    console.log("Please enter a non-negative integer");
-                } else {
-                    let fact = 1;
-
-                    for (let i = 1; i <= num; i++) {
-                        fact *= i;
-                    }
-
-                    console.log("The factorial of " + num + " is " + fact);
-                }
-            })
-
-
-
-
-
-
+            computeFactorial();
         } else if (choice === "3") {
             console.log("Exiting Application");
         } else {
@@ -51,13 +29,32 @@ function mainMenu() {
         }
 
     })
+
 }
 
 function sayHello() {
     console.log("Hello");
 }
 
+function computeFactorial() {
+    console.log("Factorial");
 
+    rl.question("Please enter a number for factorial: ", numStr => {
+        let num = parseInt(numStr);
+
+        if (isNaN(num) || num < 0) {
+            console.log("Please enter a non-negative integer");
+        } else {
+            let fact = 1;
+
+            for (let i = 1; i <= num; i++) {
+                fact *= i;
+            }
+
+            console.log("The factorial of " + num + " is " + fact);
+        }
+    })
+}
 
 
 mainMenu();
